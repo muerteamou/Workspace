40acc0dd76a3eeaa437c76988cbf4ea464c34ff2
DAM1ALU7 <DAM1ALU7@B01-PC07.IESGUARA.LOCAL>
2022-05-13 14:21:56 +0200

    13 mayo

--- Ud2/src/ejercicios/Ejercicio16.java
+++ Ud2/src/ejercicios/Ejercicio16.java
@@ -19,30 +19,22 @@ public class Ejercicio16 {
 		System.out.println("Introduce un número, vamos a calcular la suma de sus dígitos:");
 		Scanner s = new Scanner(System.in);
 		/*
-		int x = s.nextInt(), suma = 0, cociente = 0;
-		while (x > 10) {
-			cociente = (x % 10);
-			suma = suma + cociente;
-			x = x / 10;
-		}
-		suma = suma + x;
-		System.out.println("La suma de sus digitos es " + suma);
+		 * int x = s.nextInt(), suma = 0, cociente = 0; while (x > 10) { cociente = (x %
+		 * 10); suma = suma + cociente; x = x / 10; } suma = suma + x;
+		 * System.out.println("La suma de sus digitos es " + suma);
 		 */
-		
-		int suma=0;
+
+		int suma = 0;
 		Integer x = s.nextInt();
 		String cadena = Integer.toString(x);
-		
-		for(int i =0; i< cadena.length(); i++) {
-			//suma = suma + Integer.parseInt(cadena.charAt(i));
-			//System.out.println(Integer.valueOf(cadena.charAt(i)));
+
+		for (int i = 0; i < cadena.length(); i++) {
+			// suma = suma + Integer.parseInt(cadena.charAt(i));
+			// System.out.println(Integer.valueOf(cadena.charAt(i)));
 			suma = suma + Character.getNumericValue(cadena.charAt(i));
 		}
 		System.out.println(suma);
 		s.close();
-		
-		
-		
 	}
 
 }
